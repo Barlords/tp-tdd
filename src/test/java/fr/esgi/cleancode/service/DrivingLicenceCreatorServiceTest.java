1c963545f6350e279de00a3bd34794c4f5a88297
Barlords <basile.pulin@gmail.com>
2022-11-26 00:19:06 +0100

    TDD for creator service

--- src/test/java/fr/esgi/cleancode/service/DrivingLicenceCreatorServiceTest.java
+++ src/test/java/fr/esgi/cleancode/service/DrivingLicenceCreatorServiceTest.java
@@ -1,10 +1,8 @@
 package fr.esgi.cleancode.service;
 
 import fr.esgi.cleancode.database.InMemoryDatabase;
-import fr.esgi.cleancode.model.DrivingLicence;
 import org.junit.jupiter.api.Test;
 
-import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
