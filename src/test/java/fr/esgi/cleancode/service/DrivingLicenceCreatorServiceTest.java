8f5d2455666c8e2a7a404b4894ed79652a569d81
Barlords <basile.pulin@gmail.com>
2022-11-26 00:18:58 +0100

    TDD for creator service

--- src/test/java/fr/esgi/cleancode/service/DrivingLicenceCreatorServiceTest.java
+++ src/test/java/fr/esgi/cleancode/service/DrivingLicenceCreatorServiceTest.java
@@ -1,13 +1,24 @@
 package fr.esgi.cleancode.service;
 
+import fr.esgi.cleancode.database.InMemoryDatabase;
+import fr.esgi.cleancode.model.DrivingLicence;
 import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class DrivingLicenceCreatorServiceTest {
 
-    @Test
-    void should_create_and_save_driving_licence()
-    {
+    private final InMemoryDatabase db = InMemoryDatabase.getInstance();
+    private final DrivingLicenceCreatorService creatorService = new DrivingLicenceCreatorService(db);
 
+    @Test
+    void should_create_and_save_driving_licence_in_database()
+    {
+        var given = creatorService.create("123456789123456");
+        var actual = db.findById(given.getId());
+        assertThat(actual).containsSame(given);
     }
 
 }
