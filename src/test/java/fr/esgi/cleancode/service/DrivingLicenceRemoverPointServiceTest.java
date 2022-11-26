7a79bba0b896a9849deb83175d0abf2023dc1290
Barlords <basile.pulin@gmail.com>
2022-11-26 01:37:59 +0100

    Coverage 100% WTF!!!!!!

--- src/test/java/fr/esgi/cleancode/service/DrivingLicenceRemoverPointServiceTest.java
+++ src/test/java/fr/esgi/cleancode/service/DrivingLicenceRemoverPointServiceTest.java
@@ -2,6 +2,7 @@ package fr.esgi.cleancode.service;
 
 import fr.esgi.cleancode.database.InMemoryDatabase;
 import fr.esgi.cleancode.model.DrivingLicence;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -29,11 +30,20 @@ public class DrivingLicenceRemoverPointServiceTest {
         final var id = UUID.randomUUID();
         final var given = DrivingLicence.builder().id(id).build();
         final var modified = given.withAvailablePoints(10);
-        when(database.findById(id)).thenReturn(Optional.ofNullable(given));
+        when(database.findById(id)).thenReturn(Optional.of(given));
         when(database.save(id, modified)).thenReturn(modified);
         var actual = service.removePoint(id, 2);
 
         assertThat(given.getAvailablePoints()-2).isEqualTo(actual.getAvailablePoints());
+    }
+
+    @Test
+    void should_not_found_licence()
+    {
+        final var id = UUID.randomUUID();
+        when(database.findById(id)).thenReturn(Optional.empty());
+        Assertions.assertThatException().isThrownBy(() -> service.removePoint(id, 2));
+
     }
 
 }
